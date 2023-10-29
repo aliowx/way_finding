ca8cc6fca502d7723835ef3048fbedde9db67427
Amir Zare <mailwork368@gmail.com>
2023-10-29 16:02:36 +0330

    fix: Change celery config

--- app/app/core/celery_app.py
+++ app/app/core/celery_app.py
@@ -3,9 +3,16 @@ from celery import Celery
 from app.core.config import settings
 
 
-BROKER_URL = f"amqp://{settings.RABBITMQ_USERNAME}:{settings.RABBITMQ_PASSWORD}@{settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}"
-print(f"---------{BROKER_URL}-----")
-celery_app = Celery("worker", backend="rpc://", broker=BROKER_URL)
+BROKER_URL = (
+    f"amqp://{settings.RABBITMQ_USERNAME}:"
+    f"{settings.RABBITMQ_PASSWORD}@"
+    f"{settings.RABBITMQ_HOST}:"
+    f"{settings.RABBITMQ_PORT}"
+)
 
-celery_app.conf.task_routes = {"app.celery.worker.test_celery": "main-queue"}
-celery_app.conf.update(task_track_started=True)
+
+celery_app = Celery(
+    "worker", backend="rpc://", broker=BROKER_URL, include=["celery.worker"]
+)
+
+celery_app.conf.update(task_track_started=True, broker_connection_retry_on_startup=True)
