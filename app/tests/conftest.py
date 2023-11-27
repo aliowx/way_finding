e76746b4bcd4f157430f41a7a42459f1e0c45b35
Amir Zare <mailwork368@gmail.com>
2023-11-27 14:25:24 +0330

    feat: Add util functionality and crud tests

---
+++ app/tests/conftest.py
@@ -0,0 +1,69 @@
+import asyncio
+from typing import Dict, Generator
+
+import pytest
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from httpx import AsyncClient
+
+from app.core.config import settings
+from app.main import app
+from app.db import Base
+from tests.utils.user import authentication_token_from_email
+from tests.utils.utils import get_superuser_token_headers
+
+
+engine = create_async_engine(
+    settings.POSTGRES_TEST_DB,
+)
+async_session_maker = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False,
+)
+
+
+@pytest.fixture(scope="session")
+def event_loop(request) -> Generator:  # noqa: indirect usage
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="module")
+async def db() -> AsyncSession:
+    async_engine = engine
+    async_session = async_session_maker
+
+    async with async_session() as s:
+        async with async_engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+        yield s
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        pass
+
+    await async_engine.dispose()
+
+
+@pytest_asyncio.fixture(scope="module")
+async def client() -> AsyncClient:
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
+
+
+@pytest_asyncio.fixture(scope="module")
+async def superuser_token_headers(client: AsyncClient) -> Dict[str, str]:
+    return await get_superuser_token_headers(client)
+
+
+@pytest_asyncio.fixture(scope="module")
+async def normal_user_token_headers(
+    client: AsyncClient, db: AsyncSession
+) -> dict[str, str]:
+    return await authentication_token_from_email(
+        client=client, email=settings.EMAIL_TEST_USER, db=db
+    )
