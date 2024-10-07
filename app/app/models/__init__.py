d2c741143d24b8301afa8e0c4a8c9810de8f2c8d
kamranabdi <kamranabdicse@gmail.com>
2024-10-07 16:50:11 +0330

    add vertex and edge model

--- app/app/models/__init__.py
+++ app/app/models/__init__.py
@@ -1,2 +1,4 @@
 from .request_log import RequestLog
 from .user import User
+from .vertex import Vertex
+from .edge import Edge
---
+++ app/app/models/vertex.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Boolean, Integer, String
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from app.db.base_class import Base
+
+
+class Vertex(Base):
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, autoincrement=True, index=True
+    )
+    x: Mapped[int] = mapped_column(Integer, nullable=True)
+    y: Mapped[int] = mapped_column(Integer, nullable=True)
+    name: Mapped[str] = mapped_column(String(20), nullable=True)
+    description: Mapped[str] = mapped_column(String(50), nullable=True)
+
+    source_edges: Mapped[list["Edge"]] = relationship(
+        "Edge", foreign_keys="[Edge.source_vertex_id]", back_populates="source_vertex"
+    )
+    destination_edges: Mapped[list["Edge"]] = relationship(
+        "Edge",
+        foreign_keys="[Edge.destination_vertex_id]",
+        back_populates="destination_vertex",
+    )
