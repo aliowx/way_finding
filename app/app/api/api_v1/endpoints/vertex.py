532c18eb268bbdebccc3f4f60b6584e04dd0f8ba
alialibakhshi <alialibakhshi9898@gmail.com>
2024-10-22 10:35:54 +0100

    handel some chalange

--- app/app/api/api_v1/endpoints/vertex.py
+++ app/app/api/api_v1/endpoints/vertex.py
@@ -5,7 +5,8 @@ from app.api import deps
 from sqlalchemy import select
 from app import exceptions as exc
 from app import schemas
-from app.api.api_v1.services.vertex import VertexService 
+from app.api.api_v1.services import vertex
+
 router = APIRouter()
 namespace = "Position"
 
@@ -16,22 +17,13 @@ async def create_vertax(
     *,
     vertex_in: schemas.VertexCreate,
 ):
-    vertx_servis = VertexService(db)
-
     try:
-        new_vertex = await vertx_servis.register_position(
-            endx=vertex_in.endx,
-            endy=vertex_in.endy,
-            startx=vertex_in.startx,
-            starty=vertex_in.starty,
-            pox=vertex_in.pox,
-            poy=vertex_in.poy,
+        new_vertex = await vertex.register_position(
+            db=db,input=vertex_in
         )
-        return new_vertex
-    except HTTPException as e:
+    except Exception as e:
         raise e
-
-
+    return new_vertex
 
 
 @router.get("/")
