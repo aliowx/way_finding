218f4bdd830b0ebc65f958eeb9372888d2d7c2dc
ali <alialibakhhshi9898@gmail.com>
2024-11-26 13:33:52 +0330

    make a test/vertex

--- app/tests/api/api_v1/test_vertex.py
+++ app/tests/api/api_v1/test_vertex.py
@@ -8,60 +8,63 @@ from fastapi.testclient import TestClient
 
 @pytest.mark.asyncio
 class TestVertex:
+    endx: float = 100.5,
+    endy: float = 200.5,
+    startx: float = 10.5,
+    starty: float = 20.5,
+    pox: float = 1.0,
+    poy: float = 2.0  
+    
+    @property
+    def data(self):
+        return {
+        'endx': TestVertex.endx,
+        'endy': TestVertex.endy,
+        'startx': TestVertex.startx,
+        'starty': TestVertex.starty,
+        'pox': TestVertex.pox,
+        'poy': TestVertex.poy       
+    }
 
-    async def test_create_vertex_post(client: AsyncClient):
-        vertex_data = VertexCreate(
-                endx = 100.5,
-                endy = 200.5,
-                startx = 10.5,
-                starty = 20.5,
-                pox = 1.0,
-                poy = 2.0  
+     
+    async def test_register_input(
+        self,
+        client: AsyncClient
+    )-> None:
+        # normal input
+        
+        response = await client.post(
+            f"{settings.API_V1_STR}/",
+            json=self.data
+        )   
+        assert response.status_code == 200
+    
+    # duplicate input
+    
+        response = await client.post(
+            f"{settings.API_V1_STR}/",
+            json=self.data
         )
+        assert response.status_code == 409
 
-        api_url = f"{settings.API_V1_STR}/create_vertex/"
 
-        response = client.post(
-            api_url,
-            json=vertex_data.model_dump(),
-            auth=BasicAuth(
-                username=settings.HEALTH_USERNAME,
-                password=settings.HEALTH_PASSWORD 
-            )
-        )
+    async def test_multiple_requstes(
+        self,
+        client: AsyncClient
+    )-> None:
+        for i in range(10):
+            response = await client.post(
+                f'{settings.API_V1_STR}/',
+                json=self.data
+            )    
+        try:
+            assert response.status_code == 200
+        except:                
+            assert response.status_code == 404
 
-        assert response.status_code == status.HTTP_200_OK
-        response_data = response.json()
-        created_vertex_id = response_data.get('id')
-        return created_vertex_id
-
-    async def test_create_vertex_get(client:AsyncClient, vertex_id:int):
-            
-        api_url = f"{settings.API_V1_STR}vertices/{vertex_id}/"
-
-        response = await client.get(
-            api_url,
-            auth=BasicAuth(
-                username=settings.HEALTH_USERNAME,
-                password=settings.HEALTH_PASSWORD 
-            )
-        )
-
-        assert response.status_code == status.HTTP_200_OK
-
-        response_data = response.json()
-        assert response_data == vertex_id
-
-    async def test_create_vertex_del(client:AsyncClient,id:int):
-
-        api_url = f"{settings.API_V1_STR}/{id}"
-
-        response = await client.delete(
-            api_url,
-            auth=BasicAuth(
-                username=settings.HEALTH_USERNAME,
-                password=settings.HEALTH_PASSWORD 
-            )
-        )
-
-        assert response.status_code == status.HTTP_404_NOT_FOUND
+    async def test_missing_data(
+        self,
+        client: AsyncClient
+    )-> None:
+        response = await client.get(f'{settings.API_V1_STR}/')
+        assert response.status_code == 400
