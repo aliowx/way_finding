2c92ae32c669af770da598f5761f4897bffd5738
ali <alialibakhhshi9898@gmail.com>
2024-11-26 12:46:26 +0330

    Fixed server handling and changes

--- app/initial_data.py
+++ app/initial_data.py
@@ -10,15 +10,14 @@ logger = logging.getLogger(__name__)
 
 async def create_init_data() -> None:
     async with async_session() as session:
-        # await init_db(session)
-        await shortest_path(session)
+        await init_db(session)
+        # await shortest_path(session)
 
 
 async def main() -> None:
     logger.info("Creating initial data")
     await create_init_data()
     logger.info("Initial data created")
-
 
 if __name__ == "__main__":
     asyncio.run(main())
