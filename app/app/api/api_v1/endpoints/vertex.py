85b6caf3f54779440b2dd94823ce07bb938ff7d5
alialibakhshi <alialibakhshi9898@gmail.com>
2025-04-10 04:51:32 -0400

    fix: fix the conflicts

--- app/app/api/api_v1/endpoints/vertex.py
+++ app/app/api/api_v1/endpoints/vertex.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends 
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from app import crud, models, schemas
 from app.api import deps
@@ -8,6 +9,27 @@ from sqlalchemy import select
 from app import exceptions as exc
 from app import schemas
 from app.api.api_v1.services.vertex import VertexService 
+from app.api.api_v1.services import vertex
+
+router = APIRouter()
+namespace = "Position"
+
+
+@router.post("/")
+async def create_vertax(
+    db: AsyncSession = Depends(deps.get_db_async),
+    *,
+    vertex_in: schemas.VertexCreate,
+):
+    try:
+        new_vertex = await vertex.register_position(
+            db=db,input=vertex_in
+        )
+    except Exception as e:
+        raise e
+    return new_vertex
+
+
 router = APIRouter()
 namespace = "Position"
 
